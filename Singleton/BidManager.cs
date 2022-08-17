722e14ba242ff7d7092cd7cca2a3442cf78a336b
Vedat ERMIS <vedatermis@hotmail.com>
2022-08-17 20:47:16 +0300

    commit

---
+++ Singleton/BidManager.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton;
+
+public class BidManager
+{
+    private static BidManager _bidManager;
+    private static object _lockObject = new();
+    private BidManager() { }
+    
+    public static BidManager CreateAsSingleton()
+    {
+        lock (_lockObject)
+        {
+            _bidManager ??= new BidManager();
+        }
+
+        return _bidManager;
+    }
+    
+    public void Save()
+    {
+        Console.WriteLine("Saved");
+    }
+}
